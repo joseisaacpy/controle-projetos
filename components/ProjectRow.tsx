e7482f5b0d9fc81bb10e6ecdb398e3cc6dfd7490
José Isaac <joseisaacnascimento@gmail.com>
2025-12-01 16:49:34 -0300

    feat(projectRow): criar função para editar projeto - criar modal de edição com dialog

--- components/ProjectRow.tsx
+++ components/ProjectRow.tsx
@@ -1,15 +1,44 @@
 "use client";
 
+import { useState } from "react";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import { Edit, Trash } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectItem,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import formatDate from "@/utils/formatDate";
+import { Edit, Trash } from "lucide-react";
 import type { Projeto } from "@prisma/client";
 import { toast } from "sonner";
 
 export default function ProjectRow({ projeto }: { projeto: Projeto }) {
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  // url da api
+  const API_URL = "/api/projetos";
+
+  // estado para controlar o formulário
+  const [form, setForm] = useState({
+    nome: projeto.nome,
+    slug: projeto.slug,
+    linkProjeto: projeto.linkProjeto,
+    ativo: projeto.ativo,
+    mensagemBloqueio: projeto.mensagemBloqueio ?? "",
+  });
   // função para deletar um projeto
   async function handleDelete() {
     try {
@@ -24,6 +53,27 @@ export default function ProjectRow({ projeto }: { projeto: Projeto }) {
     } catch (error) {
       console.error(error);
       toast.error("Erro ao deletar projeto");
+    }
+  }
+  // função para editar um projeto
+  async function handleEdit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${API_URL}/${projeto.slug}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao editar projeto");
+      }
+      toast.success("Projeto editado com sucesso");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao editar projeto");
     }
   }
   return (
@@ -49,8 +99,7 @@ export default function ProjectRow({ projeto }: { projeto: Projeto }) {
           )}
         </TableCell>
         <TableCell className="text-center">
-          {/* se tem mensagem, mostrar badge vermelho, se não, mostrar badge
-                  sem mensagem */}
+          {/* se tem mensagem, mostrar badge vermelho, se não, mostrar badge sem mensagem */}
           {projeto.mensagemBloqueio ? (
             <Badge variant="destructive">{projeto.mensagemBloqueio}</Badge>
           ) : (
@@ -65,10 +114,93 @@ export default function ProjectRow({ projeto }: { projeto: Projeto }) {
         </TableCell>
         {/* célula de ações */}
         <TableCell className="text-center flex justify-center gap-1">
-          {/* botão de editar */}
-          <Button>
-            <Edit />
-          </Button>
+          {/* modal de edição */}
+          <Dialog>
+            {/* trigger */}
+            <DialogTrigger asChild>
+              {/* botão que abre o modal */}
+              <Button>
+                <Edit />
+              </Button>
+            </DialogTrigger>
+            {/* conteúdo do modal */}
+            <DialogContent>
+              <form onSubmit={handleEdit} className="space-y-5">
+                {/* header */}
+                <DialogHeader>
+                  <DialogTitle>Editar Projeto</DialogTitle>
+                </DialogHeader>
+                <div className="grid gap-4">
+                  <div className="form-group">
+                    <Label>Nome</Label>
+                    <Input
+                      value={form.nome}
+                      onChange={(e) =>
+                        setForm({ ...form, nome: e.target.value })
+                      }
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <Label>Slug</Label>
+                    <Input
+                      value={form.slug}
+                      onChange={(e) =>
+                        setForm({ ...form, slug: e.target.value })
+                      }
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <Label>Link</Label>
+                    <Input
+                      value={form.linkProjeto}
+                      onChange={(e) =>
+                        setForm({ ...form, linkProjeto: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="form-group">
+                    <Label>Status</Label>
+                    <Select
+                      value={form.ativo ? "ativo" : "inativo"}
+                      onValueChange={(value) =>
+                        setForm({ ...form, ativo: value === "ativo" })
+                      }
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="ativo">Ativo</SelectItem>
+                        <SelectItem value="inativo">Inativo</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  <div className="form-group">
+                    <Label>Mensagem de bloqueio</Label>
+                    <Input
+                      value={form.mensagemBloqueio}
+                      onChange={(e) =>
+                        setForm({
+                          ...form,
+                          mensagemBloqueio: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                </div>
+
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline">Cancelar</Button>
+                  </DialogClose>
+                  <Button type="submit">Salvar</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
           {/* botão de deletar */}
           <Button variant="destructive" onClick={handleDelete}>
             <Trash />
